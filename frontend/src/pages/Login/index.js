ad523a6976457ac2aa4ec2cc1c95830c26eda3a0
Felipe Alvarenga <felipedialvarenga07@gmail.com>
2021-11-03 21:43:44 -0300

    feat: autenticacao de usuario implementada na tela de Login #12

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,26 +1,58 @@
-import React from "react";
-import FormContainer from '../../components/FormContainer';
-import Header from '../../components/Header';
-import './styles.css'
-import {FaRegEnvelope, FaLock} from 'react-icons/fa'
+import React, { useState } from "react";
+import {useHistory} from 'react-router-dom'
+import FormContainer from "../../components/FormContainer";
+import Header from "../../components/Header";
+import "./styles.css";
+import { FaRegEnvelope, FaLock } from "react-icons/fa";
 import FormInput from "../../components/FormInput";
+import { toast } from "react-toastify";
 
 const Login = () => {
-    return (
-        <div className="container">
-            <Header/>
-            <FormContainer title="Login" buttonTitle="Entrar">
-                <div className="inputRow">
-                    <FaRegEnvelope size="30"/>
-                    <FormInput type="text" placeholder="Email" />
-                </div>
-                <div className="inputRow">
-                    <FaLock size="30"/>
-                    <FormInput type="password" placeholder="Senha" />
-                </div>
-            </FormContainer>
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const history = useHistory();
+
+  const loginHandler = async () => {
+    try {
+      const response = await fetch("http://localhost:3003/users/auth", {
+        method: "POST",
+        mode: 'cors',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      
+      if (response.ok) {
+        toast.success("(✿◡‿◡) Usuário logado! ");
+      } else {
+        const data = await response.json();
+        toast.error(` ( •̀ ω •́ )y ${data.error}`)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  return (
+    <div className="container">
+      <Header />
+      <FormContainer title="Login" buttonTitle="Entrar" onClick={loginHandler}>
+        <div className="inputRow">
+          <FaRegEnvelope size="30" />
+          <FormInput type="text" placeholder="Email" value={enteredEmail} onChange={e => setEnteredEmail(e.target.value)}/>
         </div>
-    )
-}
+        <div className="inputRow">
+          <FaLock size="30" />
+          <FormInput type="password" placeholder="Senha" value={enteredPassword} onChange={e => setEnteredPassword(e.target.value)}/>
+        </div>
+      </FormContainer>
+    </div>
+  );
+};
 
 export default Login;
