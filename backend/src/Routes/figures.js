780b976a0f9a68e8a0deb04867b9acf5dd7f58ff
“Italo <italo.queiroz@ykt.com.br>
2021-12-02 19:12:37 -0300

    Fix: req params on get figures method

--- backend/src/Routes/figures.js
+++ backend/src/Routes/figures.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router();
 const figures = require('../model/figures');
 
-router.get('/', async (req, res) => {
-    const { email } = req.body;
+router.get('/:email', async (req, res) => {
+    const { email } = req.params;
 
     if (!email) return res.status(400).send({ error: ' Houve um erro na busca por produtos' })
     try {
